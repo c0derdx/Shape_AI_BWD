5e709bfa017969309cfe84a1dbbf5b26745c9220
c0derdx <75431371+c0derdx@users.noreply.github.com>
2021-06-06 06:45:41 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+return (
+    <div className="note">
+    <h1>Army of the Dead</h1>
+    <p>
+    Following a zombie outbreak in Las Vegas, a group of mercenaries take the ultimate gamble: venturing into the quarantine zone to pull off the greatest heist ever attempted.
+    </p>
+    </div>
+);
+}
+
+export default Note;
